1ed0656f6f401af12a462f981b318a66916eb7eb
chrzanxq <marcinch199@gmail.com>
2022-12-18 13:30:11 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import Header from "./components/header/Header.jsx";
+import Nav from "./components/nav/Nav.jsx";
+import About from "./components/about/About.jsx";
+import Stack from "./components/stack/Stack.jsx";
+import Portfolio from "./components/portfolio/Portfolio.jsx";
+import Contact from "./components/contact/Contact.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import "./index.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Stack />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
