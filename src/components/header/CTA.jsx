6b245d698889a038c18bce5003b5caafaa513719
chrzanxq <marcinch199@gmail.com>
2022-12-18 14:34:50 +0100

    Built Out 1st component

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CV from "../../assets/images/CV.pdf";
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={CV} download>
+        Download CV
+      </a>
+      <a href="#contact">Contact Me</a>
+    </div>
+  );
+};
+
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import CTA from "./CTA";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello my name is</h5>
+        <h2>Marcin Chrzanowski</h2>
+        <h5 className="text-light">Front End Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
