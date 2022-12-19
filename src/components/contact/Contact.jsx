6cf76e25fa52e9ebe6febd48f1795e1b15de2038
chrzanxq <marcinch199@gmail.com>
2022-12-19 21:35:43 +0100

    added portfolio and contact forms

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsWhatsapp, BsInstagram } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact"> Contact </section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me!</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <a href="mailto:marcinch199@gmail.com" target="_blank">
+              marcinch199@gmail.com
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <a
+              href="https://api.whatsapp.com/send?phone=48739222907"
+              target="_blank"
+            >
+              +48-739-222-907
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <BsInstagram className="contact__option-icon" />
+            <h4>Instagram</h4>
+            <a href="https://www.instagram.com/chrzanxq/" target="_blank">
+              chrzanxq
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send!
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,85 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/images/portfolio/portfolio1.png";
+import IMG2 from "../../assets/images/portfolio/portfolio2.png";
+import IMG3 from "../../assets/images/portfolio/portfolio3.png";
 
 const Portfolio = () => {
-  return <section id="portfolio"> Portfolio </section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="1st portfolio picture" />
+          </div>
+
+          <h3>Dice Game</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/chrzanxq/pickgame"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://chrzanxq.github.io/pickgame/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="2nd portfolio picture" />
+          </div>
+          <h3>Number Guessing Game</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/chrzanxq/numguess"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://chrzanxq.github.io/numguess/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="3rd portfolio picture" />
+          </div>
+          <h3>Random Quote Generator</h3>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/chrzanxq/adviceapp"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://639b76fed8a2a473d98aa7a9--graceful-cat-09eb17.netlify.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
